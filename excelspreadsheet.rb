270ccfcff37d64734b4e8b93d5f7e2fddbc6402a
PhilipPeters <35991743+PhilipPeters@users.noreply.github.com>
2020-11-12 13:28:48 +0000

    Add files via upload

---
+++ excelspreadsheet.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Compiled version of /Users/rebeccapeters/downloads/rubbish.xlsx
+require '/Users/rebeccapeters/code/scripts/etc/excel_to_code/src/excel/excel_functions'
+
+class Excelspreadsheet
+  include ExcelFunctions
+  def original_excel_filename; "/Users/rebeccapeters/downloads/rubbish.xlsx"; end
+  attr_accessor :sheet1_a1 # Default: 1000.0
+  def sheet1_a2; @sheet1_a2 ||= add(sheet3_a1,1.0); end
+  def sheet1_b2; @sheet1_b2 ||= add(sheet3_a1,2.0); end
+  def sheet2_a1; @sheet2_a1 ||= add(sheet1_a1,1.0); end
+  def sheet3_a1; @sheet3_a1 ||= add(sheet2_a1,1.0); end
+
+# Start of named references
+# End of named references
+
+  # starting initializer
+  def initialize
+    @sheet1_a1 = 1000.0
+  end
+
+end
